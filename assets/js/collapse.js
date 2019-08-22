9acf3d70c7d6123b3ce0a66ffdd55287914d9d90
Adrian Law <alaw@bu.edu>
2019-08-22 00:03:55 -0400

    added functionality and styling for expand/collapse sections

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,14 +1,59 @@
 var coll = document.getElementsByClassName("section-title");
 var i;
+var clickall = document.getElementById('clickall');
+var displayall = false;
+
+function showSection(section) {
+	section.classList.add('active');
+	section.nextElementSibling.style.display = "block";
+};
+
+function hideSection(section) {
+	section.classList.remove('active');
+	section.nextElementSibling.style.display = "none";
+};
+
+function checkSectionActive(section) {
+	return section.classList.contains('active') 
+};
+function checkSectionInactive(section) {
+	return !(section.classList.contains('active'))
+};
+
 
 for (i = 0; i < coll.length; i++) {
 	coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
 		var content = this.nextElementSibling;
 		if (content.style.display === "block") {
-			content.style.display = "none";
+			hideSection(this);
 		} else {
-			content.style.display = "block";
-		}
+			showSection(this);
+		};
+
+		if (Array.from(coll).every(checkSectionActive)) {
+			displayall = true;
+			clickall.classList.add('active')
+		}; 
+		if (Array.from(coll).every(checkSectionInactive)) {
+			displayall = false;
+			clickall.classList.remove('active')
+		};
 	});
 } 
+
+clickall.addEventListener("click", function() {
+	console.log(displayall)
+	displayall = !(displayall);
+	if (displayall) {
+		this.classList.add("active");
+	} else {
+		this.classList.remove("active");
+	}
+	for (i = 0; i < coll.length; i++) {
+		if (displayall) {
+			showSection(coll[i]);
+		} else {
+			hideSection(coll[i]);
+		}
+	};
+});
